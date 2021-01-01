7f551ca955813be8c2e7abd1fd3e700894f6f2a0
Sanket Jadhav <jsanket819@gmail.com>
2021-01-01 20:25:47 +0530

    color correction and bo shadow added

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,21 @@
- const checkbox = document.getElementById('checkboxid');
-    checkbox.addEventListener('change', () => {
-        document.body.classList.toggle('dark');
-    });
+const checkbox = document.getElementById("checkboxid");
+const nav = document.getElementById("navbar");
+const foot = document.getElementById("footer");
+var x = document.getElementsByClassName("tablecol1");
+
+var i, j;
+
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+
+  nav.classList.toggle("darknav");
+
+  foot.classList.toggle("darkfoot");
+
+  for (i = 0; i < x.length; i = i + 2) {
+    x[i].classList.toggle("mystyle");
+  }
+  for (j = 1; j < x.length; j = j + 2) {
+    x[j].classList.toggle("mystyle2");
+  }
+});
